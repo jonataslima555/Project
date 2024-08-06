46bb3c6a95369af0bc2a5094f957decbce37fcfa
Jonatas Lima <desista0404@gmail.com>
2024-08-06 17:30:26 -0300

    Adicionando templates

--- recipes/views.py
+++ recipes/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('<h1>Home</h1>')
+    return render(request, 'recipes/home.html')
 
 def about(request):
-    return HttpResponse('<h1>About</h1>')
+    return render(request, 'recipes/about.html')
 
 def contats(request):
     return HttpResponse('<h1>Contats</h1>')
